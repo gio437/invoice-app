49267729ac39b51b36f27a17295b0932b222717f
Giovanni <giomail33@gmail.com>
2024-04-05 10:59:49 -0400

    added delete button to generated contacts

--- invoice-react/src/App.js
+++ invoice-react/src/App.js
@@ -3,19 +3,43 @@ import React from 'react';
 import { useState } from 'react';
 
 function App() {
-  const [contacts, newContact] = useState([]);
+  const [contacts, nextContact] = useState([]);
 
   const getContact = () => {
+    // change all of these to ID
     const inputField = document.querySelector('.contactInputField');
     const contactInfo = inputField.value;
     generateContact(contactInfo);
+    inputField.value = '';
   }
 
-  const generateContact = (contact) => {
+  let contactCount = 0;
+  const generateContact = (contactInfo) => {
+    nextContact(prev => prev.concat(contactCount)); // used to match id with array
     const contactParent = document.querySelector('.contactList');
     const newContact = document.createElement('div');
-    newContact.textContent = contact;
+    newContact.textContent = contactInfo;
+    newContact.classList.add('contact');
+    newContact.id = contactCount;
     contactParent.append(newContact);
+    contactCount++;
+    generateDeleteBtn(contactParent, contactInfo);
+  }
+
+  const generateDeleteBtn = (contactParent, contactInfo) => {
+    if (contactInfo !== '') {
+      const newBtn = document.createElement('button');
+      newBtn.classList.add('contact');
+      newBtn.id = contactCount;
+      contactParent.append(newBtn);
+    }
+  }
+
+  const deleteContact = () => {
+    const contactList = document.querySelectorAll('.contact');
+    for (let i = 0; i < contactList.length; i++) {
+      // grab previously used function
+    }
   }
 
   return (
@@ -24,7 +48,7 @@ function App() {
         <h1>Invoice App</h1>
       </header>
       <div className='input-contacts'>
-        <label for='contact-input'>Input Contact </label>
+        <label for='contact-input'>Input Contact</label>
         <input className='contactInputField' type='text' name='contact-input' placeholder='New Contact?'></input>
         <button className='submitContactBtn' onClick={getContact}>Enter</button>
       </div>
